9350b6671ae514b161afd00763262356283abe66
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-06 15:01:35 +0300

    Font Family config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Lato', 'ui-sans-serif', 'system-ui'],
+      display: ['Joan', 'serif'],
+      body: ['Lato'],
+    },
     extend: {},
   },
   plugins: [],
