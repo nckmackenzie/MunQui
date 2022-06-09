59b9a16cfa86104fdb47d3c03052c57df6c87a16
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-09 10:05:49 +0300

    Winning functionality set up

--- src/components/quiz/Winnings.jsx
+++ src/components/quiz/Winnings.jsx
@@ -1,20 +1,8 @@
-import { useState, useEffect } from 'react';
 import { winningMultiplier } from '../../data';
 import WinningItem from './WinningItem';
 import { useQuestion } from '../../context/QuestionProvider';
 export default function Winnings() {
-  const { level } = useQuestion();
-  const [levelPoints, setLevelPoints] = useState(0);
-
-  useEffect(() => {
-    if (level === 'easy') {
-      setLevelPoints(500);
-    } else if (level === 'medium') {
-      setLevelPoints(1000);
-    } else {
-      setLevelPoints(5000);
-    }
-  }, [level]);
+  const { levelPoints } = useQuestion();
 
   return (
     <div className="basis-1/3 bg-gray-100 p-6 ">
