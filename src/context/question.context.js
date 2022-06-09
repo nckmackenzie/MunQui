7bed16609d1963fc6f667bfbabd696829bb8a971
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-09 10:57:56 +0300

    Added content value in createContext for autocompletion

--- src/context/question.context.js
+++ src/context/question.context.js
@@ -1,5 +1,9 @@
 import { createContext } from 'react';
 
-const QuestionContext = createContext({ level: '', questionNo: '' });
+const QuestionContext = createContext({
+  level: '',
+  questionNo: '',
+  levelPoints: '',
+});
 
 export default QuestionContext;
