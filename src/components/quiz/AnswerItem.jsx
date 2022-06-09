4892dee32f2b9acd4797993db0914853d942b21c
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-09 09:57:38 +0300

    Answer Item component and functionality

--- src/components/quiz/AnswerItem.jsx
+++ src/components/quiz/AnswerItem.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import { useRef } from 'react';
 
 export default function AnswerItem({ answer, onClick }) {
+  const answerRef = useRef();
+  const onClickHandler = () => {
+    const selectedAnswer = String(answerRef.current.innerText).trim();
+    onClick(selectedAnswer);
+  };
   return (
     <li
-      onClick={onClick}
-      className="w-full px-2 py-1 bg-white border border-solid border-stone-400 cursor-pointer transition-all hover:bg-stone-400"
+      onClick={onClickHandler}
+      ref={answerRef}
+      className="w-full px-2 py-1 bg-white border border-solid border-stone-400 cursor-pointer transition-all hover:bg-stone-400 hover:text-gray-900"
     >
       {answer}
     </li>
