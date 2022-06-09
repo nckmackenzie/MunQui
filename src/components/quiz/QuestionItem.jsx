083cb87359b485f6e37125a50ed06fff85314c51
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-09 09:58:11 +0300

    Question Item component and functionality

--- src/components/quiz/QuestionItem.jsx
+++ src/components/quiz/QuestionItem.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import AnswerItem from './AnswerItem';
 import { useQuestion } from '../../context/QuestionProvider';
 
@@ -9,15 +10,24 @@ export default function QuestionItem({
   questionNo,
   index,
 }) {
+  let navigate = useNavigate();
   const { setQuestionNo } = useQuestion();
-  const onClickHandler = () => {
-    setQuestionNo(questionNo + 1);
+  const onClickHandler = answer => {
+    if (questionNo < 10) {
+      if (answer === correct) {
+        setQuestionNo(questionNo + 1);
+      } else {
+        navigate('/results?win=false', { replace: true });
+      }
+    } else {
+      navigate('/results?win=true', { replace: true });
+    }
   };
 
   return (
     <article
       className={`px-8 pt-4 absolute top-1/2 -translate-y-1/2 transition-all ${
-        index === questionNo ? 'opacity-100' : 'opacity-0'
+        index === questionNo ? 'block' : 'hidden'
       }`}
     >
       <header className="mb-8">
