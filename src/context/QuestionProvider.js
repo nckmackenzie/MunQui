a26ced12719a08bfa1ee496757809d073fad2164
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-06-09 10:04:49 +0300

    Level points state management

--- src/context/QuestionProvider.js
+++ src/context/QuestionProvider.js
@@ -1,17 +1,28 @@
-import { useContext, useState, useCallback } from 'react';
+import { useContext, useState, useCallback, useEffect } from 'react';
 import QuestionContext from './question.context';
 
 const QuestionProvider = ({ children }) => {
   const [questionNo, setQuestionNo] = useState(1);
   const [level, setLevel] = useState();
+  const [levelPoints, setLevelPoints] = useState(0);
 
   const changeLevel = useCallback(level => {
     setLevel(level);
   }, []);
 
+  useEffect(() => {
+    if (level === 'easy') {
+      setLevelPoints(500);
+    } else if (level === 'medium') {
+      setLevelPoints(1000);
+    } else {
+      setLevelPoints(5000);
+    }
+  }, [level]);
+
   return (
     <QuestionContext.Provider
-      value={{ questionNo, setQuestionNo, level, changeLevel }}
+      value={{ questionNo, setQuestionNo, level, changeLevel, levelPoints }}
     >
       {children}
     </QuestionContext.Provider>
